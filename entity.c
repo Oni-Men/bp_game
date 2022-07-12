c528cf4646842e9cd23b64f17740c309c26d643c
Oni-Men <sensyaheis@gmail.com>
2022-07-13 00:00:57 +0900

    some

--- entity.c
+++ entity.c
@@ -1,7 +1,3 @@
 #include "entity.h"
 
-void initEntity(Entity *e) { e->id = 0; }
-
-void saveEntity(Entity *entity, const char *path) {}
-
-int onGround(Entity *e) { return 0; }
+void initEntity(Entity *e) {}
--- entity.h
+++ entity.h
@@ -1,7 +1,6 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include "map.h"
 #include "space.h"
 #include "status.h"
 
@@ -10,17 +9,9 @@ typedef struct {
   Space2d space;
   Vec2d velocity;
   Status status;
-  Tile *hitTile;
-  int onGround;
   double rotation;
 } Entity;
 
 void initEntity(Entity *entity);
-
-void saveEntity(Entity *entity, const char *path);
-
-int onGround(Entity *e);
-
-void updateEntity(Entity *e);
 
 #endif
